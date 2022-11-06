6436a859fc62cfd136069be8f8010881bf5c0c74
stfackk <denkis2205@gmail.com>
2022-11-06 19:37:22 +0700

    v7

---
+++ lab/src/main/java/example/lab/Controller/MyRunnables.java
@@ -0,0 +1,32 @@
+package example.lab.Controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+
+class MyRunnables implements Runnable {
+
+
+    @FXML
+    ProgressBar pBar;
+
+    private double progress;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(20);
+                progress = i / 1000;
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        pBar.setProgress(progress);
+                    }
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
